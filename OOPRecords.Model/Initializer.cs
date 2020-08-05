12201fae6c7efe1db26816cb40d3fabf145c62fe
Richard Pawson <rpawson@nakedobjects.net>
2020-08-05 15:28:20 +0100

    StudentNumber added

--- OOPRecords.Model/Initializer.cs
+++ OOPRecords.Model/Initializer.cs
@@ -3,21 +3,32 @@ using System.Data.Entity;
 
 namespace OOPRecords.Model
 {
-    public class Initializer : DropCreateDatabaseIfModelChanges<DatabaseContext>
+    public class Initializer : DropCreateDatabaseAlways<DatabaseContext>
     {
         protected override void Seed(DatabaseContext context)
         {
             var students = context.Students;
-            var jj = NewStudent(students, "James", "Java", "24/03/2004");
-            var aa = NewStudent(students, "Alie", "Algol", "19/02/2004");
-            var ff = NewStudent(students, "Forrest", "Fortran", "22/09/2003");
-            NewStudent(students, "Simon", "Simula", "1/11/2003");
+            var jj = NewStudent(students, "James", "Java", "24/03/2004", "HW531");
+            var aa = NewStudent(students, "Alie", "Algol", "19/02/2004", "HM287");
+            var ff = NewStudent(students, "Forrest", "Fortran", "22/09/2003", "LX046");
+            NewStudent(students, "Simon", "Simula", "1/11/2003", "LY112");
             var teachers = context.Teachers;
             var hh = NewTeacher(teachers, "Harry", "Haskell", "Computer Science teacher");
             var bb = NewTeacher(teachers, "Bunny", "Bunsen", "Head of Chemistry");
             jj.Tutor = hh;
             aa.Tutor = hh;
             ff.Tutor = bb;
+        }
+
+        private Student NewStudent(DbSet<Student> students, string firstName, string lastName, string dob, string number)
+        {
+            var s = new Student();
+            s.FirstName = firstName;
+            s.LastName = lastName;
+            s.DateOfBirth = Convert.ToDateTime(dob);
+            s.StudentNumber = number;
+            students.Add(s);
+            return s;
         }
 
         private Teacher NewTeacher(DbSet<Teacher> teachers, string firstName, string lastName, string jobTitle)
@@ -28,16 +39,6 @@ namespace OOPRecords.Model
             t.JobTitle = jobTitle;
             teachers.Add(t);
             return t;
-        }
-
-        private Student NewStudent(DbSet<Student> students, string firstName, string lastName, string dob)
-        {
-            var s = new Student();
-            s.FirstName = firstName;
-            s.LastName = lastName;
-            s.DateOfBirth = Convert.ToDateTime(dob);
-            students.Add(s);
-            return s;
         }
     }
 }
